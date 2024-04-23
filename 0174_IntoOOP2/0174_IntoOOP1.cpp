ca661b1075679d0a519ce16d5f1456edeaa8bdd9
thekill022 <gibfathoni@gmail.com>
2024-04-23 11:47:55 +0700

    membuat member dari class mahasiswa

--- 0174_IntoOOP2/0174_IntoOOP1.cpp
+++ 0174_IntoOOP2/0174_IntoOOP1.cpp
@@ -4,7 +4,10 @@ using namespace std;
 class Mahasiswa { //membuat class mahasiswa
 
     public: //akses modifier public
-
+        //membuat member dari class mahasiswa
+        string nama;
+        int umur;
+        string jurusan;
 };
 
 
