2f04e9394539722718cc9764074cb444571cdf7c
thekill022 <gibfathoni@gmail.com>
2024-04-23 11:59:53 +0700

    membuat akses modifier public untuk class bangunDatar

--- 0174_IntoOOP/0174_IntoOOP.cpp
+++ 0174_IntoOOP/0174_IntoOOP.cpp
@@ -6,6 +6,8 @@ class bangunDatar { //membuat class bangunDatar
     private: //akses modifier private
         //membuat member private dari class bangunDatar
         float panjang, lebar;
+
+    public : //akses modifier public
 };
 
 int main()
